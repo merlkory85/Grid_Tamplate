c05381bfe2661f1b6aa1afa89bc391d0f28ebc80
Yavorskyi Serhii <merlkory1985@gmai.com>
2021-09-20 00:17:15 +0300

    додав js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
-$(document).ready(function(){
-  $('.header-navbar__btn').on('click', function () {
-    $(this).toggleClass('header-navbar__btn--active');
-    $('.header-navbar__list').stop(true, true).slideToggle(500);
+$(document).ready(function () {
+  $(".header-navbar__btn").on("click", function () {
+    $(this).toggleClass("header-navbar__btn--active");
+    $(".header-navbar__list").stop(true, true).slideToggle(500);
   });
 });
+
+// $(document).ready(function () {
+//   $(".header-navbar__btn").click(function () {
+//     $(this).toggleClass("header-navbar__btn--active");
+//   });
+// });
